f0aab4a78ce1f38e12e73c6bc1ae9f0216f440a7
jaydeep17 <jaydp17@gmail.com>
2016-10-16 01:33:09 +0530

    expose providers too

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 'use strict';
 
 module.exports.Queue = require('./src/queue');
+module.exports.AzureProvider = require('./src/providers/azure');
+module.exports.AwsProvider = require('./src/providers/aws');
